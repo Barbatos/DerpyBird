a179a7e67342eda9f3378a3504f760082be7f911
Bardoll <t.labroue@gmail.com>
2014-03-27 10:28:59 +0100

    Add random moves to derpy bird

--- DerpyBird/src/com/me/GameObjects/Bird.java
+++ DerpyBird/src/com/me/GameObjects/Bird.java
@@ -11,6 +11,7 @@ public class Bird {
 	private float rotation; 
 	private int width;
 	private int height;
+	private int rand=1;
 	
 	private boolean moving;
 	
@@ -57,8 +58,13 @@ public class Bird {
     }
 
     public void onClick() {
-        velocity.y = -140;
+    	
+    	while(rand < 80){
+    	 rand = (int) (Math.random()*180);}
+    	System.out.println(rand);
+        velocity.y = -(rand);
         moving = true;
+        rand = 1;
     }
     
     public boolean isFalling() {
